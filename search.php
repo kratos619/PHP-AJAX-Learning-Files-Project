7d310a3e67e19c6a394fd61873b524f1c109a45f
kratos619 <gaurav.palaspagar@hotmail.com>
2018-06-28 19:36:18 +0530

    add database query

--- search.php
+++ search.php
@@ -2,8 +2,22 @@
 
 $connection = mysqli_connect('localhost','root','','ajax');
 
-if($connection){
-    echo "is Connected";
+// if($connection){
+//     echo "is Connected";
+// }
+
+
+
+$search = mysqli_real_escape_string($connection ,$_POST['autocomplete_input']);
+
+if(!empty($search)){
+    $query = "SELECT * FROM cars WHERE car LIKE '$search%'";
+    $search_query = mysqli_query($connection,$query);
+    if(!$search_query){
+        die('query failed ' . mysqli_error($connection));
+    }
+
 }
+
 
 ?>
