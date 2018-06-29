39d9f71cfacf08feeaf8568bf30aa11e089a39ca
kratos619 <gaurav.palaspagar@hotmail.com>
2018-06-29 09:57:13 +0530

    Display All cars

--- display_cars.php
+++ display_cars.php
@@ -1,4 +1,24 @@
 <?php
 include('ini.php');
 
+$query = "SELECT * FROM cars";
+
+$result = mysqli_query($connection,$query);
+if(!$result){
+    die("something Went Wrong");
+}
+while($row = mysqli_fetch_assoc($result)){
+    $cars_id = h($row['id']);
+    $cars_name = h($row['car']);
+
+    ?>
+    <tr>
+        <td><?php echo h($cars_id); ?></td>
+        <td><?php echo h($cars_name); ?></td>
+    </tr>
+
+    <?php
+}
+//redirect_to("display_cars.php");
+
 ?>
