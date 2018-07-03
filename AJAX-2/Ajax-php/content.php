846ee91a674825fe65d9ad0ccfd8ae16f81e03a3
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-03 12:13:32 +0530

    Ajax  with PHP-Json

---
+++ AJAX-2/Ajax-php/content.php
@@ -0,0 +1,10 @@
+<?php
+$content = array(
+    'short' => 'new content',
+    'regular' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, distinctio.",
+    'long' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id placeat voluptas velit maxime officia, veniam minus officiis repellat neque."
+);
+
+echo json_encode($content);
+
+?>
